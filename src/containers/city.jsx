e0bba265541e6fb32ff1128997f33ae5c29adb90
matt <mattevo10@gmail.com>
2021-01-10 18:23:51 +0000

    changes to city

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -7,14 +7,11 @@ const City = (props) => {
   const handleClick = () => {
     props.selectCity(props.city);
   };
-
   return (
     <li className="list-group-item" onMouseOver={handleClick}>{props.city.name}</li>
   );
 };
-
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ selectCity }, dispatch);
 }
-
 export default connect(null, mapDispatchToProps)(City);
